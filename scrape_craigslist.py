ac853cf4eb22fa90e0cb222f0ed3afbbd3268beb
Nick Ulle <nick.ulle@gmail.com>
2014-06-26 01:38:32 -0700

    Modify comments a bit.

--- scrape_craigslist.py
+++ scrape_craigslist.py
@@ -1,32 +1,20 @@
 '''
-Scrape Davis housing data from Craigslist
+Scrape Davis housing data from Craigslist.
 
 Try running this script and then examining each of the
 four intermediate data structures.
 '''
 
-import requests
-import bs4
 import csv
 
-davis_housing_url = ('http://sacramento.craigslist.org/search/'
-                     'apa?query=davis&sale_date=-')
-
-# Make the network call
-craig_response = requests.get(davis_housing_url)
-
-# Parse the response using BeautifulSoup
-craig_soup = bs4.BeautifulSoup(craig_response.content)
-
-# Look in the 'p' tag elements to find just the actual listings
-ptags = craig_soup.find_all('p')
-
+import requests
+import bs4
 
 def extract(tag):
     '''
     Extract relevant info from a single HTML tag.
-    try / except block performs error handling for missing data
     '''
+    # try / except block performs error handling for missing data
     try:
         price = tag.find('span', {'class': 'price'}).text.replace('$', '')
         loc = tag.small.text
@@ -35,14 +23,9 @@ def extract(tag):
     except AttributeError:
         return ('', '', '')
 
-
-# Apply the extract function to every element in ptags
-housing = map(extract, ptags)
-
-
-def to_csv(content=housing, filename='davis_housing.csv'):
+def to_csv(content, filename):
     '''
-    Write extracted results to a CSV file
+    Write extracted results to a CSV file.
     '''
     with open(filename, 'w') as target:
         writer = csv.writer(target)
@@ -53,7 +36,29 @@ def to_csv(content=housing, filename='davis_housing.csv'):
         # Write in the main data
         writer.writerows(content)
 
+def main():
+    '''
+    Run the script.
+    '''
+    # Use parentheses to expressions across multiple lines
+    davis_housing_url = ('http://sacramento.craigslist.org/search/'
+                         'apa?query=davis&sale_date=-')
+
+    # Make the network call
+    craig_response = requests.get(davis_housing_url)
+
+    # Parse the response using BeautifulSoup
+    craig_soup = bs4.BeautifulSoup(craig_response.content)
+
+    # Look in the 'p' tag elements to find just the actual listings
+    ptags = craig_soup.find_all('p')
+
+    # Apply the extract function to every element in ptags
+    housing = map(extract, ptags)
+
+    to_csv(housing, 'davis_housing.csv')
 
 # Statements inside this block will not run if the module is imported
 if __name__ == '__main__':
-    to_csv()
+    main()
+
