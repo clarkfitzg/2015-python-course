bec8db6fe52e6a14225bc84e50664d7ff4388f10
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-06-25 20:19:46 -0700

    Added comments for explanation

--- scrape_craigslist.py
+++ scrape_craigslist.py
@@ -1,5 +1,8 @@
 '''
 Scrape Davis housing data from Craigslist
+
+Try running this script and then examining each of the
+four intermediate data structures.
 '''
 
 import requests
@@ -7,40 +10,50 @@ import bs4
 import csv
 
 davis_housing_url = ('http://sacramento.craigslist.org/search/'
-        'apa?query=davis&sale_date=-')
+                     'apa?query=davis&sale_date=-')
 
+# Make the network call
 craig_response = requests.get(davis_housing_url)
 
-soup = bs4.BeautifulSoup(craig_response.content) 
+# Parse the response using BeautifulSoup
+craig_soup = bs4.BeautifulSoup(craig_response.content)
+
+# Look in the 'p' tag elements to find just the actual listings
+ptags = craig_soup.find_all('p')
 
 
 def extract(tag):
     '''
-    Extract relevant info from a single HTML tag
+    Extract relevant info from a single HTML tag.
+    try / except block performs error handling for missing data
     '''
     try:
-        price = tag.find('span', {'class':'price'}).text.replace('$', '')
+        price = tag.find('span', {'class': 'price'}).text.replace('$', '')
         loc = tag.small.text
-        desc = tag.find('span', {'class':'pl'}).a.text
+        desc = tag.find('span', {'class': 'pl'}).a.text
         return price, loc, desc
     except AttributeError:
         return ('', '', '')
 
 
-ptags = soup.find_all('p')
+# Apply the extract function to every element in ptags
+housing = map(extract, ptags)
 
-def to_csv(filename='price.csv'):
+
+def to_csv(content=housing, filename='davis_housing.csv'):
     '''
     Write extracted results to a CSV file
     '''
-    with open(filename, 'w') as price:
-        writer = csv.writer(price)
+    with open(filename, 'w') as target:
+        writer = csv.writer(target)
+
+        # Add a row for the header
         writer.writerow(('price', 'location', 'description'))
-        for tag in ptags:
-            writer.writerow(extract(tag))
 
-# Above a map could be used rather than the for loop
-# writer.writerows(map(extract, ptags))
+        # Write in the main data
+        writer.writerows(content)
 
+
+# Statements inside this block will not run if the module is imported
 if __name__ == '__main__':
     to_csv()
