39b381036d96e79417b2aadc81974d7dd7d5ace2
Clark Fitzgerald <clarkfitzg@gmail.com>
2014-07-18 13:59:03 -0700

    including visualization created by graphviz

--- latex_paper/make_graph.py
+++ latex_paper/make_graph.py
@@ -10,25 +10,45 @@ command line usage:
 '''
 
 
+# accumulate the nodes here so that we can write the labels
+nodeset = set()
+
+
 def printdot(line):
     '''
     Prints dot graph output for a single line in Makefile
     '''
-    targets, depends = line.split(':')
+    # dot language doesn't like . character in nodenames
+    targets, depends = line.replace('.', '_').split(':')
     targets = targets.split()
     depends = depends.split()
     
     for t in targets:
+        nodeset.add(t)
         for d in depends:
-            print(' -> '.join((t, d)), ';')
+            nodeset.add(d)
+            print(' -> '.join((d, t)))
 
 
-print('digraph {')
+print('digraph {\nrankdir = BT')
+#print('digraph {')
 
+# Graph structure
 with open('Makefile') as makefile:
     for line in makefile:
         # Skips comments and commands
         if line[0].isalpha():
             printdot(line)
+
+# Node labels
+
+print()
+
+for node in nodeset:
+    index = node.rfind('_')
+    if index != -1:
+        label = '.'.join([node[:index], node[index + 1:]])
+        label = ''.join(['[label="', label, '"]'])
+        print(node, label)
 
 print('}')
